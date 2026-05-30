38530678d1ab11faad2c312b1fd985c5bffc025d
chengege666 <609906658@qq.com>
2026-05-30 11:42:13 +0800

    1

--- store.sh
+++ store.sh
@@ -17,7 +17,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 . "${SCRIPT_DIR}/plugins/luci-theme-argon.sh"
 . "${SCRIPT_DIR}/plugins/taskplan.sh"
 . "${SCRIPT_DIR}/plugins/passwall2.sh"
-. "${SCRIPT_DIR}/plugins/smartdns.sh"
+[ -f "${SCRIPT_DIR}/plugins/smartdns.sh" ] && . "${SCRIPT_DIR}/plugins/smartdns.sh"
 
 TTY="/dev/tty"
 
